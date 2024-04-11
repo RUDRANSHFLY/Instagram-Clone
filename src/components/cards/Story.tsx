69990c80dc203cda2238a5669385b459689ff08e
RUDRANSH BARADIYA <rokudokubaradiyabrothers@gmail.com>
2024-04-11 09:50:30 +0530

    Single Story Component Add

---
+++ src/components/cards/Story.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React from "react";
+
+interface profileStory {
+  id: number;
+  bio: string;
+  userName: string;
+  profilePic: string;
+  userSecondName: string;
+}
+
+const Story = ({
+  bio,
+  id,
+  profilePic,
+  userName,
+  userSecondName,
+}: profileStory) => {
+  return (
+    <div className="">
+      <div className="relative h-14 w-14">
+        <Image
+          src={profilePic}
+          fill={true}
+          className="absolute object-contain rounded-full cursor-pointer p-1 border-2 border-red-700 sm:hover:scale-110 transform transition-all duration-200 ease-out"
+          alt="Story-User"
+        />
+      </div>
+      <p className="text-xs truncate text-center">{userName}</p>
+    </div>
+  );
+};
+
+export default Story;
