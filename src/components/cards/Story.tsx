79a237d30b55244b6d4abe98da98fec8fc9f3f25
RUDRANSH BARADIYA <rokudokubaradiyabrothers@gmail.com>
2024-04-21 09:09:44 +0530

    Minor Change in Image

--- src/components/cards/Story.tsx
+++ src/components/cards/Story.tsx
@@ -20,13 +20,17 @@ const Story = ({
     <div className="">
       <div className="relative h-14 w-14">
         <Image
-          src={profilePic}
+          src={profilePic || ""}
+          sizes={"100vw,100vh"}
           fill={true}
-          className="absolute object-contain rounded-full cursor-pointer p-1 border-2 border-red-700 sm:hover:scale-110 transform transition-all duration-200 ease-out"
-          alt="Story-User"
+          className={
+            "absolute object-contain rounded-full cursor-pointer p-1 border-2 border-red-700 sm:hover:scale-110 transform transition-all duration-200 ease-out"
+          }
+          priority={true}
+          alt={"Story-User"}
         />
       </div>
-      <p className="text-xs truncate text-center">{userName}</p>
+      <p className={"text-xs truncate text-center"}>{userName || "User"}</p>
     </div>
   );
 };
