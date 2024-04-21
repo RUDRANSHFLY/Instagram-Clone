1f576f59a4934bdabee3419018ae0ba6d3682e78
RUDRANSH BARADIYA <rokudokubaradiyabrothers@gmail.com>
2024-04-21 09:08:35 +0530

    Model Compoenet Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Feed from "@/components/main/Feed";
 import NavBar from "@/components/navbar/NavBar";
+import Model from "@/model/Model";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
+      <Model />
       <NavBar />
       <Feed />
     </div>
